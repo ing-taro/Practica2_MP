cce4dd3fc077ecc6a70002f5197358388884509f
unknown <neizanroggie7@gmail.com>
2025-11-23 18:36:59 +0100

    commit inicial

---
+++ src/libClases/Cliente.java
@@ -0,0 +1,5 @@
+package libClases;
+
+public class Cliente {
+
+}
---
+++ src/libPruebas/Prueba.java
@@ -0,0 +1,10 @@
+package libPruebas;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

4c8c9853ee21fcd5226be3dc12aef3e7b4d3e623
unknown <neizanroggie7@gmail.com>
2025-11-23 19:45:29 +0100

    implemento el metodo pedirfecha()

--- src/libClases/Fecha.java
+++ src/libClases/Fecha.java
@@ -1,39 +1,162 @@
 package libClases;
 
+import java.util.Scanner;
+
 public class Fecha {
 	
 	private int dia;
 	private int mes;
 	private int anio;
 	
-	public Fecha(int dia, int mes, int anio) {
-		
-		this.dia = dia;
-		
-		this.mes = mes;
-		
-		this.anio = anio;
-		
-	}
 	
 	public void setFecha(int d, int m, int a) {
 		
 		int dmax;
 		
-		int diames[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int diaMes[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
 		this.anio = a;
 		
+		if (m<1) //si el mes es incorrecto
+			m=1;
 		
+			else if (m>12) //si el mes es incorrecto
+			m=12;
 		
+			dmax=diaMes[m-1];
+			
+			if (m==2 && bisiesto())
+			dmax++;
+			
+			if (d>dmax)
+			d=dmax;
+			
+			else if (d<1)
+			d=1;
+			
+			
+			dia=d;
+			
+			mes=m;
+			
+	}
 		
+	
+	public Fecha(int dia, int mes, int anio) {
+		
+		setFecha(dia, mes, anio);
 		
 	}
 	
-	public int getDia(int d) { return this.dia; }
 	
-	public int getMes(int m) { return this.mes; }
+	public Fecha (Fecha f) {
+		
+		dia = f.dia;
+		
+		mes = f.mes;
+		
+		anio = f.anio;
+		
+	}
 	
-	public int getAnio(int a) { return this.anio; }
+	
+
+	public String toString() {
+		
+		String s="";
+		
+		if (dia<10) s=s+0;
+		
+		s=s+dia+"/";
+		
+		if (mes<10) s=s+0;
+		
+		s=s+mes+"/"+anio;
+		
+		return s;
+
+		}
+	
+	public int getDia() { return this.dia; }
+	
+	public int getMes() { return this.mes; }
+	
+	public int getAnio() { return this.anio; }
+	
+	
+	public boolean bisiesto() {
+		
+		if((this.anio % 4 == 0 & this.anio % 100 != 0) || this.anio % 400 == 0) {
+			
+			return true;
+		}
+		else 
+			
+			return false;
+	}
+	
+	
+	public void ver() {
+		
+		System.out.println(this/*.toString()*/);
+		
+		}
+	
+	
+	
+	public static Fecha pedirFecha() {
+		
+		Fecha fecha = null;
+		
+		boolean valida = false;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int dia, mes, anio;
+		
+		do {
+			
+		System.out.println("Introduce la Fecha (dd/mm/aaaa): ");
+		
+		String cadena = sc.next();
+		
+		String[] tokens = cadena.split("/");
+		
+		try {
+		if (tokens.length != 3)
+			
+		throw new NumberFormatException();
+		
+		dia = Integer.parseInt(tokens[0]); //parseInt lanza la excepcion
+		
+		mes = Integer.parseInt(tokens[1]); //NumberFormatException si no
+		
+		anio = Integer.parseInt(tokens[2]);//puede convertir el String a int
+		
+		fecha = new Fecha(dia, mes, anio);
+		
+		if (fecha.getDia() != dia || fecha.getMes() != mes)
+		throw new NumberFormatException();
+		
+		valida=true;
+		
+		} 
+		catch(NumberFormatException e) {
+			
+		System.out.println("Fecha no valida");
+		
+		}
+		
+		} 
+		while(!valida);
+		
+		sc.close();
+		
+		return fecha;
+		
+	}
+	
+	
+	
 
 }

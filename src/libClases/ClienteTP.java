770b9c14b4befc65446aac657ae915cfcd897eba
unknown <neizanroggie7@gmail.com>
2025-11-26 19:56:37 +0100

    creo los atributos de la clase y los inicializo

--- src/libClases/ClienteTP.java
+++ src/libClases/ClienteTP.java
@@ -2,4 +2,17 @@ package libClases;
 
 public class ClienteTP {
 
+
+private static float tarifa = 0;
+
+private static float limiteMinutos = 300;
+
+private static float coste = 0.15f;
+
+private float minutosHablados = 0;
+
+private String nacionalidad = " ";
+
+
+
 }

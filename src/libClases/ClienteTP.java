8e316510ad63d7dfc08ecfe50fc5bd97cba18034
unknown <neizanroggie7@gmail.com>
2025-11-26 19:58:33 +0100

    creamos el constructor por defecto y el de copia

--- src/libClases/ClienteTP.java
+++ src/libClases/ClienteTP.java
@@ -1,17 +1,40 @@
 package libClases;
 
-public class ClienteTP {
+public class ClienteTP extends Cliente{
+	
+	//atributos
+
+	private static float tarifa = 0;
+	
+	private static float limiteMinutos = 300;
+	
+	private static float coste = 0.15f;
+	
+	private float minutosHablados = 0;
+	
+	private String nacionalidad = " ";
+	
+	
+	//constructores
+	
+	public ClienteTP (String NIF, String nom, Fecha fnac, Fecha fAlta, int m, String nac) {
+		
+		super(NIF, nom, fnac, fAlta); //constructor del padre
+		
+		this.minutosHablados = m;
+		
+		this.nacionalidad = nac;
+		
+	}
+	
+	public ClienteTP (Cliente c) {
+		
+		super(c);
+	}
+	
+	
 
 
-private static float tarifa = 0;
-
-private static float limiteMinutos = 300;
-
-private static float coste = 0.15f;
-
-private float minutosHablados = 0;
-
-private String nacionalidad = " ";
 
 
 

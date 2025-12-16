ecfdc9583838a5be31160eb71aea4b14b71598a6
unknown <neizanroggie7@gmail.com>
2025-12-16 13:48:40 +0100

    creo una nueva clase que hereda de TarifaPlana junto a sus metodos

--- src/libClases/ClienteSatelitalinternet.java
+++ src/libClases/ClienteSatelitalinternet.java
@@ -2,6 +2,16 @@ package libClases;
 
 public class ClienteSatelitalinternet extends ClienteTarifaPlana{
 	
+	//atributos
 	
+		private float gigas;
+		
+		private static float limiteGigas = 200.0f; //gigas base
+		
+		private static float tarifaBase = 35.0f; //precio de los 200GB base
+		
+		private static float gigaExtra = 0.10f; //precio giga extra
+		
+		
 
 }
